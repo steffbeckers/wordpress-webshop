175a5449b3dc5a5917379d4594eeff70c7b12545
Steff Beckers <steff@steffbeckers.eu>
2020-12-29 21:33:30 +0100

    webshop update

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'webshop' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '123' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
